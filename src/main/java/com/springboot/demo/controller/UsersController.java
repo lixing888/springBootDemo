673bc85472b857b215efe2f837297158ee8dd1b8
Administrator <85210279@qq.com>
2019-02-13 11:41:56 +0800

    代码更新

--- src/main/java/com/springboot/demo/controller/UsersController.java
+++ src/main/java/com/springboot/demo/controller/UsersController.java
@@ -1,13 +1,20 @@
 package com.springboot.demo.controller;
 
+import cn.hutool.http.HttpUtil;
 import com.springboot.demo.entity.Users;
 import com.springboot.demo.service.UsersService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.util.Json;
+import net.sf.json.JSON;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @Api(tags = "/用户管理")
@@ -15,6 +22,8 @@ public class UsersController {
 
     @Autowired
     private UsersService usersService;
+    @Value("${url}")
+    private String url;
 
     @RequestMapping(value = "users/list",method = RequestMethod.GET)
     public String UserInfo(){
@@ -35,6 +44,11 @@ public class UsersController {
     @PostMapping(value = "saveUserInfo")
     @ApiOperation(value = "保存用户信息",notes="ID不能为空")
     public Users  saveUserInfo(@RequestBody Users users){
+        Map<String,Object> map=new HashMap<>();
+        map.put("username","lixing");
+        String post= HttpUtil.post(url,map);
+        JSONObject resultJson= JSONObject.fromObject(post);
+
 
         usersService.insertUsers(users);
         return users;
---
+++ src/main/java/com/springboot/demo/util/ResolveJson.java
@@ -0,0 +1,28 @@
+package com.springboot.demo.util;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+public class ResolveJson {
+
+    public static void main(String[] args) {
+        String json = "{\"data\":{\"items\":[{\"itemstring\":\"手机\",\"itemcoord\":{\"x\":0,\"y\":100,\"width\":40,\"height\":20},}],\"session_id\":\"9012321\",},\"code\":0,\"message\":\"OK\"}";
+        JSONObject jsonObject = JSONObject.fromObject(json);
+        JSONObject data = jsonObject.getJSONObject("data");
+        JSONArray items = jsonObject.getJSONObject("data").getJSONArray("items");
+        JSONObject row = null;
+        for(int i=0; i<items.size(); i++){
+            row = items.getJSONObject(i);
+            System.out.println("itemstring ：" + row.get("itemstring"));
+            JSONObject itemcoord = row.getJSONObject("itemcoord");
+            System.out.println("x：" + itemcoord.get("x"));
+            System.out.println("y：" + itemcoord.get("y"));
+            System.out.println("width：" + itemcoord.get("width"));
+            System.out.println("height：" + itemcoord.get("height"));
+        }
+        System.out.println("session_id：" + data.get("session_id"));
+        System.out.println("code：" + jsonObject.get("code"));
+        System.out.println("code：" + jsonObject.get("message"));
+    }
+
+}
