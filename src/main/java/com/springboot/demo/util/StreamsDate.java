f63ab4e1b26fdd9c3cd4108af215a052acead3b9
Administrator <85210279@qq.com>
2019-02-19 18:39:16 +0800

    多数据源配置

--- src/main/java/com/springboot/demo/util/StreamsDate.java
+++ src/main/java/com/springboot/demo/util/StreamsDate.java
@@ -1,17 +1,35 @@
 package com.springboot.demo.util;
 
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.LineCaptcha;
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.ReUtil;
+import cn.hutool.cron.CronUtil;
+import cn.hutool.extra.mail.MailUtil;
+import cn.hutool.extra.qrcode.QrCodeUtil;
+import cn.hutool.extra.qrcode.QrConfig;
+import cn.hutool.extra.ssh.JschUtil;
+import cn.hutool.extra.ssh.Sftp;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.poi.excel.ExcelReader;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
+import com.springboot.demo.vo.User;
+import lombok.extern.slf4j.Slf4j;
+
+import java.awt.*;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.*;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@Slf4j
 public class StreamsDate {
     private enum Status {
         OPEN, CLOSED
@@ -96,7 +114,7 @@ public class StreamsDate {
 
         //JDK8里的时间
         final Clock clock = Clock.systemUTC();
-        System.out.println( clock.instant() );
+        System.out.println(  Date.from(clock.instant()) );
         System.out.println( clock.millis() );//毫秒
 
         final LocalDate date = LocalDate.now();
@@ -131,8 +149,101 @@ public class StreamsDate {
         System.out.println( "Duration in hours: " + duration.toHours() );
 
 
+        //请求列表页 爬虫
+        String listContent = HttpUtil.get("https://www.oschina.net/action/ajax/get_more_news_list?newsType=&p=2");
+        //使用正则获取所有标题
+        List<String> titles = ReUtil.findAll("<span class=\"text-ellipsis\">(.*?)</span>", listContent, 1);
+        for (String title : titles) {
+            //打印标题
+            System.out.println(title);
+           // log.info(title);
+        }
+
+        //定时器
+//        CronUtil.schedule("*/2 * * * * *", new Task() {
+//            public void execute() {
+//                System.out.println("Task excuted.");
+//            }
+//        });
+        //发送邮件(pass需要自主生成授权码)
+        //MailUtil.send("85210279@qq.com", "测试", "邮件来自Hutool测试", false);
+
+        //生成二维码
+        QrCodeUtil.generate("https://www.baidu.com/", 300, 300, FileUtil.file("d:/qrcode.jpg"));
 
 
+        QrConfig config = new QrConfig(300, 300);
+        // 设置边距，既二维码和背景之间的边距
+        config.setMargin(3);
+        // 设置前景色，既二维码颜色（青色）
+        config.setForeColor(Color.CYAN.getRGB());
+        // 设置背景色（灰色）
+        config.setBackColor(Color.GRAY.getRGB());
+        // 生成二维码到文件，也可以到流
+        QrCodeUtil.generate("http://hutool.cn/", config, FileUtil.file("d:/qrcodeNew.jpg"));
+
+//        Sftp sftp= JschUtil.createSftp("172.0.0.1", 22, "root", "123456");
+//        //进入远程目录
+//        sftp.cd("/opt/upload");
+//        //上传本地文件
+//        sftp.put("e:/test.jpg", "/opt/upload");
+//        //下载远程文件
+//        sftp.get("/opt/upload/test.jpg", "e:/test2.jpg");
+//        //关闭连接
+//        try {
+//            sftp.close();
+//        }catch (Exception e){
+//            log.info(e.toString());
+//        }
+        //execl文档读取poi
+//        ExcelReader reader = ExcelUtil.getReader("d:/aaa.xlsx");
+//        List<List<Object>> readAll = reader.read();
+//        for(int i=0;i<readAll.size();i++){
+//            System.out.println(readAll.get(i));
+//        }
+
+        //将行列对象写出到Excel
+        List<String> row1 = CollUtil.newArrayList("aa", "bb", "cc", "dd");
+        List<String> row2 = CollUtil.newArrayList("aa1", "bb1", "cc1", "dd1");
+        List<String> row3 = CollUtil.newArrayList("aa2", "bb2", "cc2", "dd2");
+        List<String> row4 = CollUtil.newArrayList("aa3", "bb3", "cc3", "dd3");
+        List<String> row5 = CollUtil.newArrayList("aa4", "bb4", "cc4", "dd4");
+
+        List<List<String>> rows = CollUtil.newArrayList(row1, row2, row3, row4, row5);
+
+        //通过工具类创建writer
+        ExcelWriter writer = ExcelUtil.getWriter("d:/writeTest.xlsx");
+        //通过构造方法创建writer
+        //ExcelWriter writer = new ExcelWriter("d:/writeTest.xls");
+
+        //跳过当前行，既第一行，非必须，在此演示用
+        writer.passCurrentRow();
+
+        //合并单元格后的标题行，使用默认标题样式
+        writer.merge(rows.size() - 1, "测试标题");
+        //一次性写出内容
+        writer.write(rows);
+        //关闭writer，释放内存
+        writer.close();
+
+        //==============LineCaptcha 线段干扰的验证码==============
+        //定义图形验证码的长和宽
+        LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(200, 100);
+
+        //图形验证码写出，可以写出到文件，也可以写出到流
+        lineCaptcha.write("d:/line.png");
+        //输出code
+        log.info(lineCaptcha.getCode());
+        //验证图形验证码的有效性，返回boolean值
+        lineCaptcha.verify("1234");
+
+        //重新生成验证码
+        lineCaptcha.createCode();
+        lineCaptcha.write("d:/line.png");
+        //新的验证码
+        log.info(lineCaptcha.getCode());
+        //验证图形验证码的有效性，返回boolean值
+        lineCaptcha.verify("1234");
 
     }
 
