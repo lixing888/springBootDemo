4ac46eaad60071e57856d2ea06f8ced76d05a141
Administrator <85210279@qq.com>
2019-02-21 14:40:31 +0800

    json转对象

---
+++ src/main/java/com/springboot/demo/vo/Student.java
@@ -0,0 +1,35 @@
+package com.springboot.demo.vo;
+
+public class Student {
+    //姓名
+    private String name;
+    //年龄
+    private String age;
+    //住址
+    private String address;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAge() {
+        return age;
+    }
+    public void setAge(String age) {
+        this.age = age;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    @Override
+    public String toString() {
+        return "Student [name=" + name + ", age=" + age + ", address="
+                + address + "]";
+    }
+
+
+}
