eab948aa9ec9b25b8c5d66c4262c8cb4f788e4b7
Administrator <85210279@qq.com>
2019-02-25 14:33:41 +0800

    驼峰下划线转换

---
+++ src/main/java/com/springboot/demo/util/camelToUnderline.java
@@ -0,0 +1,72 @@
+package com.springboot.demo.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * java 驼峰字符和下划线字符相互转换工具类
+ */
+public class camelToUnderline {
+
+    public static final char UNDERLINE='_';
+
+    //驼峰转下划线
+    public static String camelToUnderline(String param){
+        if (param==null||"".equals(param.trim())){
+            return "";
+        }
+        int len=param.length();
+        StringBuilder sb=new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            char c=param.charAt(i);
+            if (Character.isUpperCase(c)){
+                sb.append(UNDERLINE);
+                sb.append(Character.toLowerCase(c));
+            }else{
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+    //下划线转驼峰
+    public static String underlineToCamel(String param){
+        if (param==null||"".equals(param.trim())){
+            return "";
+        }
+        int len=param.length();
+        StringBuilder sb=new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            char c=param.charAt(i);
+            if (c==UNDERLINE){
+                if (++i<len){
+                    sb.append(Character.toUpperCase(param.charAt(i)));
+                }
+            }else{
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+    public static String underlineToCamel2(String param){
+        if (param==null||"".equals(param.trim())){
+            return "";
+        }
+        StringBuilder sb=new StringBuilder(param);
+        Matcher mc= Pattern.compile("_").matcher(param);
+        int i=0;
+        while (mc.find()){
+            int position=mc.end()-(i++);
+            //String.valueOf(Character.toUpperCase(sb.charAt(position)));
+            sb.replace(position-1,position+1,sb.substring(position,position+1).toUpperCase());
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+       String str="group_id";String str1="groupId";
+       System.out.println("下划线转驼峰："+underlineToCamel(str));
+       System.out.println("驼峰转下划线："+camelToUnderline(str1));
+
+    }
+
+}
