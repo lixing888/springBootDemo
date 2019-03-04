f8f85d88b0d52620dec5f39c23d4f2ce031bb4d9
Administrator <85210279@qq.com>
2019-03-04 14:23:38 +0800

    简化代码

--- src/main/java/com/springboot/demo/controller/TestController.java
+++ src/main/java/com/springboot/demo/controller/TestController.java
@@ -2,6 +2,8 @@ package com.springboot.demo.controller;
 
 import com.springboot.demo.vo.Address;
 import com.springboot.demo.vo.AddressCheckResult;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,18 +25,21 @@ public class TestController {
     @ResponseBody
     @RequestMapping("/address")
     public void test(){
+
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kc = ks.getKieClasspathContainer();
+        // kmodule.xml配置中ksession元素的name属性
+        KieSession kieSession = kc.newKieSession("ksession-rule");
         Address address = new Address();
         address.setPostcode("99425");
-
         AddressCheckResult result = new AddressCheckResult();
         kieSession.insert(address);
         kieSession.insert(result);
         int ruleFiredCount = kieSession.fireAllRules();
         System.out.println("触发了" + ruleFiredCount + "条规则");
-
         if(result.isPostCodeResult()){
             System.out.println("规则校验通过");
-        }
+       }
 
     }
 
