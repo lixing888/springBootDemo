e4066bbd2a142d9a099a90af6ddff665e7832d63
Administrator <85210279@qq.com>
2019-03-05 15:49:13 +0800

    flowable工作流

---
+++ flowable/src/main/java/com/example/flowable/FlowableApplication.java
@@ -0,0 +1,14 @@
+package com.example.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FlowableApplication.class, args);
+	}
+
+}
