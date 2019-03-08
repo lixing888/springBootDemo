6e1621719b219a4092a6586477592f0962a4f5c1
Administrator <85210279@qq.com>
2019-03-08 18:18:41 +0800

    idea2018

--- src/main/java/com/springboot/demo/util/StreamsDate.java
+++ src/main/java/com/springboot/demo/util/StreamsDate.java
@@ -242,7 +242,7 @@ public class StreamsDate {
         //图形验证码写出，可以写出到文件，也可以写出到流
         lineCaptcha.write("d:/line.png");
         //输出code
-        log.info(lineCaptcha.getCode());
+        //log.info(lineCaptcha.getCode());
         //验证图形验证码的有效性，返回boolean值
         lineCaptcha.verify("1234");
 
@@ -250,7 +250,7 @@ public class StreamsDate {
         lineCaptcha.createCode();
         lineCaptcha.write("d:/line.png");
         //新的验证码
-        log.info(lineCaptcha.getCode());
+        //log.info(lineCaptcha.getCode());
         //验证图形验证码的有效性，返回boolean值
         lineCaptcha.verify("1234");
 
