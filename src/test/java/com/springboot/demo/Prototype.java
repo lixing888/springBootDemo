1a33dababb2312266754e5a4d28866aa52c2d119
Administrator <85210279@qq.com>
2019-04-12 11:24:48 +0800

    原型模式就是讲一个对象作为原型,使用clone()方法来创建新的实例。

---
+++ src/test/java/com/springboot/demo/Prototype.java
@@ -0,0 +1,33 @@
+package com.springboot.demo;
+
+/**
+ * 原型模式就是讲一个对象作为原型，使用clone()方法来创建新的实例。
+ */
+public class Prototype implements Cloneable{
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected Object clone()   {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }finally {
+            return null;
+        }
+    }
+
+    public static void main ( String[] args){
+        Prototype pro = new Prototype();
+        Prototype pro1 = (Prototype)pro.clone();
+    }
+}
