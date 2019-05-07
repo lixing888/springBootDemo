001bdd734e4efd29b2ce9920d7922a3224d5eb9a
Administrator <85210279@qq.com>
2019-05-07 11:49:57 +0800

    代码优化

--- src/main/java/com/springboot/demo/util/StreamUtilExample.java
+++ src/main/java/com/springboot/demo/util/StreamUtilExample.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 /**
  * java.util.Stream使用例子
- *
+ * <p>
  * <pre>
  * java.util.Stream表示了某一种元素的序列，在这些元素上可以进行各种操作。Stream操作可以是中间操作，也可以是完结操作。
  * 完结操作会返回一个某种类型的值，而中间操作会返回流对象本身，并且你可以通过多次调用同一个流操作方法来将操作结果串起来。
@@ -18,7 +18,7 @@ import java.util.UUID;
  * public interface Stream<T> extends BaseStream<T, Stream<T>> {
  * <p>
  * 可以看到Stream是一个接口,其是1.8引入
- *
+ * <p>
  * <p>
  * Java 8中的Collections类的功能已经有所增强，你可以之直接通过调用Collections.stream()或者Collection.
  * parallelStream()方法来创建一个流对象
@@ -71,7 +71,7 @@ public class StreamUtilExample {
     /**
      * Sorted是一个中间操作，能够返回一个排过序的流对象的视图。流对象中的元素会默认按照自然顺序进行排序，
      * 除非你自己指定一个Comparator接口来改变排序规则.
-     *
+     * <p>
      * <p>
      * 一定要记住，sorted只是创建一个流对象排序的视图，而不会改变原来集合中元素的顺序。原来string集合中的元素顺序是没有改变的
      */
