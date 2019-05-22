ce84e82490761581e89d32cc39e4b07285d64c80
Administrator <85210279@qq.com>
2019-05-22 11:26:51 +0800

    jdk8Map转List

--- src/main/java/com/springboot/demo/util/MapUtilExample.java
+++ src/main/java/com/springboot/demo/util/MapUtilExample.java
@@ -1,7 +1,10 @@
 package com.springboot.demo.util;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * map是不支持流操作的。而更新后的map现在则支持多种实用的新方法，来完成常规的任务
@@ -223,5 +226,26 @@ public class MapUtilExample {
         example.remove();
         example.getOrDefault();
         example.merge();
+
+        //将Map转换成List
+        Map<Integer, String> map = new HashMap<>();
+        map.put(10, "apple");
+        map.put(20, "orange");
+        map.put(30, "banana");
+        map.put(40, "watermelon");
+        map.put(50, "dragonfruit");
+        // split a map into 2 List
+        List<Integer> resultSortedKey = new ArrayList<>();
+        List<String> resultValues = map.entrySet().stream()
+                //sort a Map by key and stored in resultSortedKey
+                .sorted(Map.Entry.<Integer, String>comparingByKey().reversed())
+                .peek(e -> resultSortedKey.add(e.getKey()))
+                .map(x -> x.getValue())
+                // filter banana and return it to resultValues
+                .filter(x -> !"banana".equalsIgnoreCase(x))
+                .collect(Collectors.toList());
+        resultSortedKey.forEach(System.out::println);
+        resultValues.forEach(System.out::println);
     }
+
 }
