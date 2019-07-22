ea63bdca4750f2675ed5319f11f245d6ee9b5092
Administrator <85210279@qq.com>
2019-07-22 10:44:40 +0800

    listSum代码格式化

--- src/main/java/com/springboot/demo/util/ListSum.java
+++ src/main/java/com/springboot/demo/util/ListSum.java
@@ -52,13 +52,13 @@ public class ListSum {
 //        });
         //log.info("批量插入数据{}",ruleRoleUsers);
         //boolean boo = ruleRoleUserManager.saveOrUpdateBatch(ruleRoleUsers);
-        log.info("执行状态{}",true);
+        log.info("执行状态{}", true);
 
-        Integer[] myArray = { 1, 2, 3 };
+        Integer[] myArray = {1, 2, 3};
         List integerList = Arrays.asList(myArray);
         System.out.println(integerList.size());
 
-        int[] intArray = { 5, 10, 21 };
+        int[] intArray = {5, 10, 21};
         List intList = Arrays.asList(myArray);
         System.out.println(intList.size());
         //Java 8 新引入的 Stream 操作
@@ -96,46 +96,65 @@ public class ListSum {
         // 交集
         List<String> intersection = list1.stream().filter(item -> list2.contains(item)).collect(toList());
         System.out.println("---交集 intersection---");
-        intersection.parallelStream().forEach(System.out :: println);
+        intersection.parallelStream().forEach(System.out::println);
 
         // 差集 (list1 - list2)
         List<String> reduce1 = list1.stream().filter(item -> !list2.contains(item)).collect(toList());
         System.out.println("---差集 reduce1 (list1 - list2)---");
-        reduce1.parallelStream().forEach(System.out :: println);
+        reduce1.parallelStream().forEach(System.out::println);
 
         // 差集 (list2 - list1)
         List<String> reduce2 = list2.stream().filter(item -> !list1.contains(item)).collect(toList());
         System.out.println("---差集 reduce2 (list2 - list1)---");
-        reduce2.parallelStream().forEach(System.out :: println);
+        reduce2.parallelStream().forEach(System.out::println);
 
         // 并集
         List<String> listAll = list1.parallelStream().collect(toList());
         List<String> listAll2 = list2.parallelStream().collect(toList());
         listAll.addAll(listAll2);
         System.out.println("---并集 listAll---");
-        listAll.parallelStream().forEachOrdered(System.out :: println);
+        listAll.parallelStream().forEachOrdered(System.out::println);
 
         // 去重并集
         List<String> listAllDistinct = listAll.stream().distinct().collect(toList());
         System.out.println("---得到去重并集 listAllDistinct---");
-        listAllDistinct.parallelStream().forEachOrdered(System.out :: println);
+        listAllDistinct.parallelStream().forEachOrdered(System.out::println);
 
         System.out.println("---原来的List1---");
-        list1.parallelStream().forEachOrdered(System.out :: println);
+        list1.parallelStream().forEachOrdered(System.out::println);
         System.out.println("---原来的List2---");
-        list2.parallelStream().forEachOrdered(System.out :: println);
+        list2.parallelStream().forEachOrdered(System.out::println);
 
 
-        List<Student> studentList =new ArrayList<>();
-        Student student1 = new Student();student1.setAge("12");student1.setSex(0);
-        Student student2 = new Student();student2.setAge("13");student2.setSex(2);
-        Student student3 = new Student();student3.setAge("11");student3.setSex(1);
-        Student student4 = new Student();student4.setAge("18");student4.setSex(1);
-        Student student5 = new Student();student5.setAge("18");student5.setSex(0);
-        Student student6 = new Student();student6.setAge("18");student6.setSex(2);
-        Student student7 = new Student();student7.setAge("18");student7.setSex(2);
-        studentList.add(student1);studentList.add(student2);studentList.add(student3);studentList.add(student4);
-        studentList.add(student5);studentList.add(student6);studentList.add(student7);
+        List<Student> studentList = new ArrayList<>();
+        Student student1 = new Student();
+        student1.setAge("12");
+        student1.setSex(0);
+        Student student2 = new Student();
+        student2.setAge("13");
+        student2.setSex(2);
+        Student student3 = new Student();
+        student3.setAge("11");
+        student3.setSex(1);
+        Student student4 = new Student();
+        student4.setAge("18");
+        student4.setSex(1);
+        Student student5 = new Student();
+        student5.setAge("18");
+        student5.setSex(0);
+        Student student6 = new Student();
+        student6.setAge("18");
+        student6.setSex(2);
+        Student student7 = new Student();
+        student7.setAge("18");
+        student7.setSex(2);
+        studentList.add(student1);
+        studentList.add(student2);
+        studentList.add(student3);
+        studentList.add(student4);
+        studentList.add(student5);
+        studentList.add(student6);
+        studentList.add(student7);
         List<Demo> demos = new ArrayList<Demo>();
 
         studentList.stream().sorted(Comparator.comparing(Student::getAge));
@@ -144,27 +163,27 @@ public class ListSum {
 
         //原始数据
         System.out.println("原始数据 组装list<demo>*******************");
-        demos = studentList.stream().map(student-> new Demo(student.getAge(),student.getSex())).collect(Collectors.toList());
+        demos = studentList.stream().map(student -> new Demo(student.getAge(), student.getSex())).collect(Collectors.toList());
         demos.forEach(demo -> {
-            System.out.println("年龄 "+demo.getAge() +"  性别 " +demo.getSex()+",");
+            System.out.println("年龄 " + demo.getAge() + "  性别 " + demo.getSex() + ",");
         });
         //只取sex为0
         System.out.println("只取sex为0****************");
-        List<Demo> demorm =demos.stream().filter(demo -> demo.getSex() == 0).distinct().collect(Collectors.toList());
+        List<Demo> demorm = demos.stream().filter(demo -> demo.getSex() == 0).distinct().collect(Collectors.toList());
         demorm.forEach(demo -> {
-            System.out.println("年龄 "+demo.getAge() +"  性别 " +demo.getSex()+",");
+            System.out.println("年龄 " + demo.getAge() + "  性别 " + demo.getSex() + ",");
         });
         //筛选年龄大于12岁
         System.out.println("筛选年龄大于12岁的*************");
-        List<Demo> demoFilter =  demos.stream().filter(demo -> Integer.valueOf(demo.getAge()) > 12).collect(Collectors.toList());
+        List<Demo> demoFilter = demos.stream().filter(demo -> Integer.valueOf(demo.getAge()) > 12).collect(Collectors.toList());
         demoFilter.forEach(demo -> {
-            System.out.println("年龄 "+demo.getAge() +"  性别 " +demo.getSex()+",");
+            System.out.println("年龄 " + demo.getAge() + "  性别 " + demo.getSex() + ",");
         });
         //排序
         System.out.println("排序******************");
         List<Demo> demoSort = demos.stream().sorted((s1, s2) -> s1.getAge().compareTo(s2.getAge())).collect(Collectors.toList());
         demoSort.forEach(demo -> {
-            System.out.println("年龄 "+demo.getAge() +"  性别 " +demo.getSex()+",");
+            System.out.println("年龄 " + demo.getAge() + "  性别 " + demo.getSex() + ",");
         });
         //倒序
         System.out.println("倒序****************");
@@ -172,26 +191,26 @@ public class ListSum {
         Comparator<Demo> comparator = (h1, h2) -> h1.getAge().compareTo(h2.getAge());
         demos.sort(comparator.reversed());
         demos.forEach(demo -> {
-            System.out.println("年龄 "+demo.getAge() +"  性别 " +demo.getSex()+",");
+            System.out.println("年龄 " + demo.getAge() + "  性别 " + demo.getSex() + ",");
         });
         //多条件正序
         System.out.println("多条件排序正序****************");
         demoArray.sort(Comparator.comparing(Demo::getSex).thenComparing(Demo::getAge));
         demoArray.forEach(demo -> {
-            System.out.println("年龄 "+demo.getAge() +"  性别 " +demo.getSex()+",");
+            System.out.println("年龄 " + demo.getAge() + "  性别 " + demo.getSex() + ",");
         });
         //多条件倒序
         System.out.println("多条件排序倒序 sex  倒序****************");
         demoArray.sort(Comparator.comparing(Demo::getSex).reversed().thenComparing(Demo::getAge));
         demoArray.forEach(demo -> {
-            System.out.println("年龄 "+demo.getAge() +"  性别 " +demo.getSex()+",");
+            System.out.println("年龄 " + demo.getAge() + "  性别 " + demo.getSex() + ",");
         });
         //按照年龄分组
         System.out.println("根据age分组结果为Map****************");
         Map<String, List<Demo>> demoOder = demos.stream().collect(Collectors.groupingBy(Demo::getAge));
         System.out.println(demoOder);
 
-        String[] myArrays = { "Apple", "Banana", "Orange" };
+        String[] myArrays = {"Apple", "Banana", "Orange"};
         List<String> myLists = new ArrayList<>();
         for (String str : myArrays) {
             myLists.add(str);
@@ -205,17 +224,17 @@ public class ListSum {
         ruleData.add(1);
         ruleData.add(0);
         ruleData.add(0);
-        List<Integer> areas=ruleData.stream().distinct().map(record -> record.intValue()).collect(Collectors.toList());
+        List<Integer> areas = ruleData.stream().distinct().map(record -> record.intValue()).collect(Collectors.toList());
 
-        List<String> list11=new ArrayList<>();
+        List<String> list11 = new ArrayList<>();
         list11.add("zzz");
         list11.add("aaa");
         list11.add("bbb");
         list11.add("bbb");
         list11.add("zzz");
-        Set<String> set=new HashSet<>(list11);
-        boolean result= list11.size() != set.size() ? true : false;
-        System.out.println( "List中是否存在重复数据:"+result);
+        Set<String> set = new HashSet<>(list11);
+        boolean result = list11.size() != set.size() ? true : false;
+        System.out.println("List中是否存在重复数据:" + result);
         Map<String, String> map = new HashMap<>();
         map.put("name", "lixing");
         map.put("age", "24");
@@ -232,22 +251,22 @@ public class ListSum {
         //return employeeInfos.stream().sorted(Comparator.comparing(DeptmentInfoVo.EmployeeInfo::getLeaderId)).collect(Collectors.toList());
         //RuleCostInfo costInfo = costInfoMap.getOrDefault(record.getCostTypeId(), new RuleCostInfo());
         //JAVA list按照另外一个给定list排序 多余的自动排在后面
-        String[] regulation = {"诸葛亮","鲁班","xzcx","貂蝉","吕布"};
+        String[] regulation = {"诸葛亮", "鲁班", "xzcx", "貂蝉", "吕布"};
         final List<String> regulationOrder = Arrays.asList(regulation);
         String[] ordered = {"诸葛亮", "nice", "貂蝉", "诸葛亮", "xzcx", "吕布", "貂蝉", "鲁班", "诸葛亮", "貂蝉", "鲁班", "诸葛亮", "hahahahah", "adsad"};
         List<String> orderedList = Arrays.asList(ordered);
-        Collections.sort(orderedList, new Comparator<String>(){
+        Collections.sort(orderedList, new Comparator<String>() {
             @Override
-            public int compare(String o1, String o2){
+            public int compare(String o1, String o2) {
                 int io1 = regulationOrder.indexOf(o1);
                 int io2 = regulationOrder.indexOf(o2);
                 if (io1 == io2) {
                     return 0;
                 }
-                if(io1 == -1){
+                if (io1 == -1) {
                     return 1;
                 }
-                if(io2 == -1){
+                if (io2 == -1) {
                     return -1;
                 }
                 return io1 - io2;
@@ -258,10 +277,6 @@ public class ListSum {
 
 
     }
-
-
-
-
 
 
     //================================
@@ -285,7 +300,7 @@ public class ListSum {
                     double gdzj1 = object2Double(map1.get("耕地"));
                     double zhj = add(zj, zj1);
                     double gdzhj = add(gdzj, gdzj1);
-                    if(!Objects.isNull(gdzhj)){
+                    if (!Objects.isNull(gdzhj)) {
                         System.out.println("非空判断");
                     }
                     Map<String, Object> map2 = new HashMap<String, Object>();
