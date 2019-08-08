d35a38d69defa481396bec53d7a9ecb94e4d689d
Administrator <85210279@qq.com>
2019-08-08 15:13:16 +0800

    代码优化

--- src/main/java/com/springboot/demo/util/StringUtils.java
+++ src/main/java/com/springboot/demo/util/StringUtils.java
@@ -106,7 +106,27 @@ public class StringUtils {
 
         System.out.println("与jar包同级："+GetServerRealPathUnit.getPath(""));
 
+        String left="左值";
+        String symbolName="符号名称";
+        String right="右值";
+        System.out.println("字符串拼接；"+String.format("%s %s %s", left, symbolName, right));
 
+        String str=null;
+        str=String.format("Hi,%s", "王力");
+        System.out.println(str);
+        str=String.format("Hi,%s:%s.%s", "王南","王力","王张");
+        System.out.println(str);
+        System.out.printf("字母a的大写是：%c %n", 'A');
+        System.out.printf("3>7的结果是：%b %n", 3>7);
+        System.out.printf("100的一半是：%d %n", 100/2);
+        System.out.printf("100的16进制数是：%x %n", 100);
+        System.out.printf("100的8进制数是：%o %n", 100);
+        System.out.printf("50元的书打8.5折扣是：%f 元%n", 50*0.85);
+        System.out.printf("上面价格的16进制数是：%a %n", 50*0.85);
+        System.out.printf("上面价格的指数表示：%e %n", 50*0.85);
+        System.out.printf("上面价格的指数和浮点数结果的长度较短的是：%g %n", 50*0.85);
+        System.out.printf("上面的折扣是%d%% %n", 85);
+        System.out.printf("字母A的散列码是：%h %n", 'A');
     }
 }
 
