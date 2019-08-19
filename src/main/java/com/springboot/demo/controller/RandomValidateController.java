5bf31aaf5a8b55485439430b5fb78cf4c41a9ef0
Administrator <85210279@qq.com>
2019-08-19 11:06:29 +0800

    随机码图片验证

---
+++ src/main/java/com/springboot/demo/controller/RandomValidateController.java
@@ -0,0 +1,67 @@
+package com.springboot.demo.controller;
+
+import com.springboot.demo.util.RandomValidateCodeUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+/**
+ * 验证随机码图片
+ * @author lixing
+ */
+@RestController
+@Slf4j
+public class RandomValidateController {
+
+    /**
+     * 生成验证码
+     */
+    @RequestMapping(value = "/getVerify")
+    public void getVerify(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            //设置相应类型,告诉浏览器输出的内容为图片
+            response.setContentType("image/jpeg");
+            //设置响应头信息，告诉浏览器不要缓存此内容
+            response.setHeader("Pragma", "No-cache");
+            response.setHeader("Cache-Control", "no-cache");
+            response.setDateHeader("Expire", 0);
+            RandomValidateCodeUtil randomValidateCode = new RandomValidateCodeUtil();
+            //输出验证码图片方法
+            randomValidateCode.getRandcode(request, response);
+        } catch (Exception e) {
+            log.error("获取验证码失败>>>>   ", e);
+        }
+    }
+
+
+    /**
+     * 忘记密码页面校验验证码
+     */
+    @RequestMapping(value = "/checkVerify", method = RequestMethod.POST, headers = "Accept=application/json")
+    public boolean checkVerify(@RequestBody Map<String, Object> requestMap, HttpSession session) {
+        try {
+            //从session中获取随机数
+            String inputStr = requestMap.get("inputStr").toString();
+            String random = (String) session.getAttribute("RANDOMVALIDATECODEKEY");
+            if (random == null) {
+                return false;
+            }
+            if (random.equals(inputStr)) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            log.error("验证码校验失败", e);
+            return false;
+        }
+    }
+
+}
