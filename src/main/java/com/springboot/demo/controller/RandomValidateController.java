0cbe2b12812a3eb0f2f90f3eb2834f7e011cba51
Administrator <85210279@qq.com>
2019-08-19 16:20:38 +0800

    代码更新

--- src/main/java/com/springboot/demo/controller/RandomValidateController.java
+++ src/main/java/com/springboot/demo/controller/RandomValidateController.java
@@ -1,6 +1,7 @@
 package com.springboot.demo.controller;
 
 import com.springboot.demo.util.RandomValidateCodeUtil;
+import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,8 @@ import java.util.Map;
  * @author lixing
  */
 @RestController
+@RequestMapping("/randomValidateController")
+@Api(tags = "验证随机码图片")
 @Slf4j
 public class RandomValidateController {
 
--- src/main/java/com/springboot/demo/vo/Address.java
+++ src/main/java/com/springboot/demo/vo/Address.java
@@ -3,13 +3,16 @@ package com.springboot.demo.vo;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 /**
  * @author lixing
  */
 @Data
 public class Address {
-    @NotBlank
+
+    @Pattern(regexp = "^[0-9a-zA-Z_]+$", message = "只能由英文数字下划线组成。")
+    @NotBlank(message = "邮政编码不能为空")
     private String postcode;
 
     private String street;
