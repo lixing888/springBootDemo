c51ed6c129d3c5c3717fda6e94e4fd4a1747dec5
lixing <85210279@qq.com>
2020-09-26 18:51:09 +0800

    双色球

--- src/test/java/com/springboot/demo/ListUtil.java
+++ src/test/java/com/springboot/demo/ListUtil.java
@@ -4,6 +4,7 @@ package com.springboot.demo;
  * @author lixing
  * java list 交集 并集 差集 去重复并集
  */
+
 import java.util.*;
 
 import static java.util.stream.Collectors.toList;
@@ -22,30 +23,36 @@ public class ListUtil {
         // 交集
         List<String> intersection = list1.stream().filter(item -> list2.contains(item)).collect(toList());
         System.out.println("---得到交集 intersection---");
-        intersection.parallelStream().forEach(System.out :: println);
+        intersection.parallelStream().forEach(System.out::println);
         // 差集 (list1 - list2)
         List<String> reduce1 = list1.stream().filter(item -> !list2.contains(item)).collect(toList());
         System.out.println("---得到差集 reduce1 (list1 - list2)---");
-        reduce1.parallelStream().forEach(System.out :: println);
+        reduce1.parallelStream().forEach(System.out::println);
         // 差集 (list2 - list1)
         List<String> reduce2 = list2.stream().filter(item -> !list1.contains(item)).collect(toList());
         System.out.println("---得到差集 reduce2 (list2 - list1)---");
-        reduce2.parallelStream().forEach(System.out :: println);
+        reduce2.parallelStream().forEach(System.out::println);
         // 并集
         List<String> listAll = list1.parallelStream().collect(toList());
         List<String> listAll2 = list2.parallelStream().collect(toList());
         listAll.addAll(listAll2);
         System.out.println("---得到并集 listAll---");
-        listAll.parallelStream().forEach(System.out :: println);
+        listAll.parallelStream().forEach(System.out::println);
         // 去重并集
-        List<String> listAllDistinct = listAll.stream().distinct().collect(toList());        System.out.println("---得到去重并集 listAllDistinct---");        listAllDistinct.parallelStream().forEach(System.out :: println);         System.out.println("---原来的List1---");        list1.parallelStream().forEach(System.out :: println);        System.out.println("---原来的List2---");        list2.parallelStream().forEach(System.out :: println);         // 一般有filter 操作时，不用并行流parallelStream ,如果用的话可能会导致线程安全问题     }
+        List<String> listAllDistinct = listAll.stream().distinct().collect(toList());
+        System.out.println("---得到去重并集 listAllDistinct---");
+        listAllDistinct.parallelStream().forEach(System.out::println);
+        System.out.println("---原来的List1---");
+        list1.parallelStream().forEach(System.out::println);
+        System.out.println("---原来的List2---");
+        list2.parallelStream().forEach(System.out::println);         // 一般有filter 操作时，不用并行流parallelStream ,如果用的话可能会导致线程安全问题     }
 
-        List list11 =new ArrayList();
+        List list11 = new ArrayList();
         list11.add("1111");
         list11.add("2222");
         list11.add("3333");
 
-        List list12 =new ArrayList();
+        List list12 = new ArrayList();
         list12.add("3333");
         list12.add("4444");
         list12.add("5555");
@@ -68,8 +75,8 @@ public class ListUtil {
         //交集
         set1.retainAll(set2);
 
-        System.out.println("交集是 "+set1);
-    
+        System.out.println("交集是 " + set1);
+
 
         //差集
         //list1.removeAll(list2);
@@ -77,7 +84,7 @@ public class ListUtil {
         list2.removeAll(list1);
         list1.addAll(list2);
 
-        Iterator<String> it=list1.iterator();
+        Iterator<String> it = list1.iterator();
         while (it.hasNext()) {
             System.out.println(it.next());
 
@@ -85,10 +92,26 @@ public class ListUtil {
 
         //System.out.println("-----------------------------------\n");
         //printStr(list1);
+        for (int i = 0; i < 5; i++) {
+            //双色球随机生成器
+            Set<String> redNbr = new TreeSet<String>();
+            while (true) {
+                int sui = (int) (Math.random() * 33) + 1;// 获取33以内的数字
+                redNbr.add(sui < 10 ? "0" + sui : "" + sui);//将元素存入集合中
+                if (redNbr.size() == 6) {//存满六个红色球，则结束循环
+                    break;
+                }
+            }
+            Set<String> blueNbr = new TreeSet<String>();
+            int sui2 = (int) (Math.random() * 16) + 1;// 获取16以内的数字
+            blueNbr.add(sui2 < 10 ? "0" + sui2 : "" + sui2);
+            System.out.println("红球：" + redNbr + " 蓝球：" + blueNbr);
+        }
+
 
     }
 
-    public static void printStr(List list1){
+    public static void printStr(List list1) {
         for (int i = 0; i < list1.size(); i++) {
             System.out.println(list1.get(i));
         }
