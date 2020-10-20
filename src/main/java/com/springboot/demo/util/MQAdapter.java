7bfd983084bd551070672c11c8102fd606c52910
lixing <85210279@qq.com>
2020-10-20 10:09:46 +0800

    适配器类

---
+++ src/main/java/com/springboot/demo/util/MQAdapter.java
@@ -0,0 +1,96 @@
+package com.springboot.demo.util;
+
+import com.alibaba.fastjson.JSON;
+import com.springboot.demo.vo.RebateInfo;
+import com.springboot.demo.vo.mq.OrderMq;
+import com.springboot.demo.vo.mq.create_account;
+
+import java.lang.reflect.InvocationTargetException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author lixing
+ *
+ */
+public class MQAdapter {
+
+    public static RebateInfo filter(String strJson, Map<String, String> link) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        return filter(JSON.parseObject(strJson, Map.class), link);
+    }
+
+    public static RebateInfo filter(Map obj, Map<String, String> link) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        RebateInfo rebateInfo = new RebateInfo();
+        for (String key : link.keySet()) {
+            Object val = obj.get(link.get(key));
+            RebateInfo.class.getMethod("set" + key.substring(0, 1).toUpperCase() + key.substring(1), String.class).invoke(rebateInfo, val.toString());
+        }
+        return rebateInfo;
+    }
+
+    public static void main(String[] args) {
+        SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date parse = null;
+        try {
+            parse = s.parse("2020-06-01 23:20:16");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+        create_account create_account = new create_account();
+        create_account.setNumber("100001");
+        create_account.setAddress("河北省.廊坊市.广阳区.大学里职业技术学院");
+        create_account.setAccountDate(parse);
+        create_account.setDesc("在校开户");
+
+        //映射字段
+        HashMap<String, String> link01 = new HashMap<String, String>();
+        link01.put("userId", "number");
+        link01.put("bizId", "number");
+        link01.put("bizTime", "accountDate");
+        link01.put("desc", "desc");
+        RebateInfo rebateInfo01 = null;
+        try {
+            rebateInfo01 = MQAdapter.filter(create_account.toString(), link01);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        System.out.println("mq.create_account(适配前)" + create_account.toString());
+        System.out.println("mq.create_account(适配后)" + JSON.toJSONString(rebateInfo01));
+
+        System.out.println("");
+
+        OrderMq orderMq = new OrderMq();
+        orderMq.setUid("100001");
+        orderMq.setSku("10928092093111123");
+        orderMq.setOrderId("100000890193847111");
+        orderMq.setCreateOrderTime(parse);
+
+        HashMap<String, String> link02 = new HashMap<String, String>();
+        //映射字段
+        link02.put("userId", "uid");
+        link02.put("bizId", "orderId");
+        link02.put("bizTime", "createOrderTime");
+        RebateInfo rebateInfo02 = null;
+        try {
+            rebateInfo02 = MQAdapter.filter(orderMq.toString(), link02);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        System.out.println("mq.orderMq(适配前)" + orderMq.toString());
+        System.out.println("mq.orderMq(适配后)" + JSON.toJSONString(rebateInfo02));
+    }
+
+}
---
+++ src/main/java/com/springboot/demo/vo/RebateInfo.java
@@ -0,0 +1,62 @@
+package com.springboot.demo.vo;
+
+import java.util.Date;
+
+/**
+ * mq消息统一返回体
+ * @author lixing
+ */
+public class RebateInfo {
+    /**
+     * 用户ID
+     */
+    private String userId;
+    /**
+     * 业务ID
+     */
+    private String bizId;
+    /**
+     * 业务时间
+     */
+    private Date bizTime;
+    /**
+     * 业务描述
+     */
+    private String desc;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getBizId() {
+        return bizId;
+    }
+
+    public void setBizId(String bizId) {
+        this.bizId = bizId;
+    }
+
+    public Date getBizTime() {
+        return bizTime;
+    }
+
+    public void setBizTime(Date bizTime) {
+        this.bizTime = bizTime;
+    }
+
+    public void setBizTime(String bizTime) {
+        this.bizTime = new Date(Long.parseLong("1591077840669"));
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
