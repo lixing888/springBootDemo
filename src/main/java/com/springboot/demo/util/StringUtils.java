7021825f936bf7ae1c730344e1f7bf2dfdbe60fe
lixing <85210279@qq.com>
2021-05-12 14:30:32 +0800

    密码强度验证

--- src/main/java/com/springboot/demo/util/StringUtils.java
+++ src/main/java/com/springboot/demo/util/StringUtils.java
@@ -17,6 +17,9 @@ public class StringUtils {
 
     private static Pattern pattern2 = Pattern.compile("[A-Z]([a-z\\d]+)?");
 
+    private final static int[] SIZE_TABLE = {9, 99, 999, 9999, 99999, 999999, 9999999, 99999999, 999999999,
+            Integer.MAX_VALUE};
+
     /**
      * 下划线转驼峰法(默认小驼峰)
      *
@@ -84,6 +87,63 @@ public class StringUtils {
     public static Boolean isObjectNotEmpty(Object obj) {
         String str = ObjectUtils.toString(obj, "");
         return org.apache.commons.lang3.StringUtils.isNotBlank(str);
+    }
+
+    /**
+     * 计算一个整数的大小
+     *
+     * @param x
+     * @return
+     */
+    public static int sizeOfInt(int x) {
+        for (int i = 0; ; i++)
+            if (x <= SIZE_TABLE[i]) {
+                return i + 1;
+            }
+    }
+
+    /**
+     * 判断字符串的每个字符是否相等
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isCharEqual(String str) {
+        return str.replace(str.charAt(0), ' ').trim().length() == 0;
+    }
+
+    /**
+     * 确定字符串是否为数字
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isNumeric(String str) {
+        for (int i = str.length(); --i >= 0; ) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 判断字符串是否为空格、空(“)”或null。
+     *
+     * @param str
+     * @return
+     */
+    public static boolean equalsNull(String str) {
+        int strLen;
+        if (str == null || (strLen = str.length()) == 0 || str.equalsIgnoreCase("null")) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if ((Character.isWhitespace(str.charAt(i)) == false)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -184,7 +244,7 @@ public class StringUtils {
             System.out.println(sql);
         }
 
-        System.out.println("String字符串是否是空:"+isObjectNotEmpty(""));
+        System.out.println("String字符串是否是空:" + isObjectNotEmpty(""));
 
     }
 }
